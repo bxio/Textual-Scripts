8e17f33da64350bbe8b02808cefa765c36dbeadd
Bill Xiong <billxiong99@gmail.com>
2014-07-10 12:05:28 -0700

    Added mode switching

---
+++ semi.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+
+  include "config.php";
+
+  if($ircUser == null){
+    echo "/debug 8,1 Please set your user and pass in config.php\n";
+  }else{
+    echo "/nick $ircUser|SemiHere";
+  }
+
+  exit();
+
+?>
