d9c47c60b1d5d27f4d888a5452f82d9974f32acf
Bill Xiong <billxiong99@gmail.com>
2014-07-10 11:19:57 -0700

    Initial Commit.

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+/** Your IRC Username */
+$ircUser = "MistyTalon";
+/** Your IRC Password */
+$ircPass = "fChfFacmvQ4yH7";
+
+?>
---
+++ m.php
@@ -0,0 +1,25 @@
+#!/usr/bin/php
+<?php
+
+  include "config.php";
+
+	$args = trim($GLOBALS['argv'][2]);
+
+
+  if($ircUser == null){
+    echo "/debug 8,1 Please set your user and pass in config.php\n";
+  }else{
+    if($args == "here"){
+      echo "/nick $ircUser";
+    }else if($args == "semi"){
+      echo "/nick $ircUser|SemiHere";
+    }else if($args == "afk" || $args == "away"){
+      echo "/nick $ircUser|AFK";
+    }else if($args == "semi"){
+      echo "/msg nickserv IDENTIFY $ircPass";
+    }
+  }
+
+	exit();
+
+?>
